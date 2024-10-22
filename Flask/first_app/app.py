d601ffd8bfb018723515d243d7be9dcea2768539
Naidan Guanin <146682331+Naidan-slv@users.noreply.github.com>
2024-10-22 19:25:38 +0000

    4. Forms Post Requests and File Handling

--- Flask/first_app/app.py
+++ Flask/first_app/app.py
@@ -1,33 +1,25 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    
-    mylist = [10,20,30,40,50]
-    return render_template('index.html', mylist=mylist)
-    # we can dynamically change this html file
-    # pass the value of myvalue and myresult to the html file
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-@app.route('/other')
-def other():
-    some_text = "Hello World"
-    return render_template('other.html',some_text=some_text)
+        if username == 'admin' and password == 'admin':
+            return 'Login Success'
+        else:
+            return 'Login Failed'#
 
 
-@app.template_filter('reverse_string')
-def reverse_string(s):
-    return s[::-1]
 
-@app.template_filter('repeat')
-def repeat(s, n):
-    return s * n
-
-#This is a redirect endpoint, it will redirect to the other endpoint which is other in this case 
-@app.route('/redirect_endpoint')
-def redirect_endpoint():
-    return redirect(url_for('other'))
+@app.route('/file upload', methods=['POST'])
+def file_upload():
+    return ''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
---
+++ Flask/first_app/read.md
@@ -0,0 +1,6 @@
+# POST Requests, File handling
+
+How to work with forms, post requests and JSON data
+
+Carry on at 12.28
+Course started looking at areas why i didnt need yet

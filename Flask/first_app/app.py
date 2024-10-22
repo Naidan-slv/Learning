b217e32ef071353bf7505c2470f82124c46bc442
Naidan Guanin <146682331+Naidan-slv@users.noreply.github.com>
2024-10-22 18:28:51 +0000

    3. Templates and HTML files (Redirect and dynamic URL paths)

--- Flask/first_app/app.py
+++ Flask/first_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder='templates')
 
@@ -9,6 +9,25 @@ def index():
     return render_template('index.html', mylist=mylist)
     # we can dynamically change this html file
     # pass the value of myvalue and myresult to the html file
+
+@app.route('/other')
+def other():
+    some_text = "Hello World"
+    return render_template('other.html',some_text=some_text)
+
+
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1]
+
+@app.template_filter('repeat')
+def repeat(s, n):
+    return s * n
+
+#This is a redirect endpoint, it will redirect to the other endpoint which is other in this case 
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+    return redirect(url_for('other'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)

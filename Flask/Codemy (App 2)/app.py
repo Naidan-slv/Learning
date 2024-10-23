083a9d3404966c6b549b8f22b393f2d1b1146c8a
Naidan Guanin <146682331+Naidan-slv@users.noreply.github.com>
2024-10-23 14:50:21 +0000

    5. WTF forms

--- Flask/Codemy (App 2)/app.py
+++ Flask/Codemy (App 2)/app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
-from Flask_wtf import FLaskForm
-from wtforms import StringFIeld, SubmitField
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+
+#create a flask instance
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')   
+app.config['SECRET_KEY'] = "Naidan"
+
+#create a form class
+
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators = [DataRequired()])
+    submit = SubmitField("Submit")
+
 
 @app.route('/')
 def index():
@@ -12,5 +22,21 @@ def index():
 @app.route('/practice')
 def practice():
     return render_template('practice.html')
+
+#create name page 
+@app.route('/name', methods =['GET','POST' ])
+def name():
+    name = None
+    form = NamerForm()
+    #Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html',
+    name = name,
+    form = form)
+
+
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug=True)
